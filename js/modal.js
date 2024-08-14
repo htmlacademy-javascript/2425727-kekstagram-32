73ad4524a91c8e877930ea68c467bf4bf925178f
Shtein365 <158086259+Shtein365@users.noreply.github.com>
2024-08-14 22:46:17 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -85,11 +85,10 @@ bigPictureCloseButton.addEventListener('click', () => {
   closeModal();
 });
 
-
 bigPictureModal.addEventListener('click', (evt) => {
-   if(evt.target === bigPictureModal) {
-     closeModal();
-   }
- });
+  if(evt.target === bigPictureModal) {
+    closeModal();
+  }
+});
 
 export {openModal, createBigPicture};
